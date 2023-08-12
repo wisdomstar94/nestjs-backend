bd6b17c73271425040dc7899456e79fc3d47205e
wisdomstar94 <wisdomstar94@gmail.com>
2023-08-12 10:48:07 +0900

    env ~ config 설정.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './controllers/test/test.module';
 
 @Module({
-  imports: [TestModule],
+  imports: [
+    TestModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/controllers/test/test.controller.ts
+++ src/controllers/test/test.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('test')
 export class TestController {
+  constructor(private configService: ConfigService) {}
+
   @Get('/')
   index() {
     return {
       name: 'hi',
       timestamp: new Date(),
+      testVariable: this.configService.get<string>('TEST_VARIABLE'),
     };
   }
 }
