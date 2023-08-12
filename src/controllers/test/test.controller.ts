bfce61ff8d6d420a9eeb394ede67865ae33ad6a4
wisdomstar94 <wisdomstar94@gmail.com>
2023-08-12 15:00:15 +0900

    파일 업로드 샘플 구현.

--- src/controllers/test/test.controller.ts
+++ src/controllers/test/test.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import {
+  AnyFilesInterceptor,
+  FileFieldsInterceptor,
+  FileInterceptor,
+} from '@nestjs/platform-express';
 
 @Controller('test')
 export class TestController {
@@ -12,5 +24,47 @@ export class TestController {
       timestamp: new Date(),
       testVariable: this.configService.get<string>('TEST_VARIABLE'),
     };
+  }
+
+  @Post('/fileUploadSingle')
+  @UseInterceptors(FileInterceptor('my_file'))
+  fileUploadSingle(@UploadedFile() my_file: Express.Multer.File) {
+    console.log('@my_file', my_file);
+    return {
+      my_file,
+    };
+  }
+
+  @Post('/fileUploadMultiple')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'my_files', maxCount: 3 },
+      { name: 'other_files', maxCount: 3 },
+    ]),
+  )
+  fileUploadMultiple(
+    @UploadedFiles()
+    fieldNamesRecord: Record<string, Array<Express.Multer.File>>,
+  ) {
+    console.log('@fieldNamesRecord', fieldNamesRecord);
+    return Object.entries(fieldNamesRecord).map(([fieldName, files]) => {
+      return {
+        fieldName,
+        files: files.map((file) => {
+          return {
+            originalname: file.originalname,
+          };
+        }),
+      };
+    });
+  }
+
+  @Post('/fileUploadAny')
+  @UseInterceptors(AnyFilesInterceptor())
+  fileUploadAny(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log('@files', files);
+    return files.map((x) => ({
+      originalname: x.originalname,
+    }));
   }
 }
